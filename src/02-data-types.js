a7797eb261b0ff0596a9b7afd084fd0f4ae666a6
Jay Jayabose <jayabose@gmail.com>
2022-02-16 06:17:18 -0500

    data types passed tests

--- src/02-data-types.js
+++ src/02-data-types.js
@@ -9,7 +9,9 @@
  *  getLengthOfString("down");
  *  //> 4
  */
-function getLengthOfString(str) {}
+function getLengthOfString(str) {
+  return str.length;
+}
 
 /**
  * convertToNumber()
@@ -22,7 +24,9 @@ function getLengthOfString(str) {}
  *  convertToNumber("111");
  *  //> 111
  */
-function convertToNumber(val) {}
+function convertToNumber(val) {
+  return parseInt(val);
+}
 
 /**
  * convertToString()
@@ -36,7 +40,9 @@ function convertToNumber(val) {}
  *  convertToString(99);
  *  //> "99"
  */
-function convertToString(val) {}
+function convertToString(val) {
+  return val.toString();
+}
 
 /**
  * convertToShoutingText()
@@ -49,7 +55,9 @@ function convertToString(val) {}
  *  convertToShoutingText("Hello There");
  *  //> "HELLO THERE"
  */
-function convertToShoutingText(text) {}
+function convertToShoutingText(text) {
+  return text.toUpperCase();
+}
 
 /**
  * convertToWhisperText()
@@ -62,7 +70,9 @@ function convertToShoutingText(text) {}
  *  convertToWhisperText("Hello There");
  *  //> "hello there"
  */
-function convertToWhisperText(text) {}
+function convertToWhisperText(text) {
+  return text.toLowerCase();
+}
 
 /**
  * checkIfCharacterIsInString()
@@ -79,7 +89,9 @@ function convertToWhisperText(text) {}
  *  checkIfCharacterIsInString("hello there", "a");
  *  //> false
  */
-function checkIfCharacterIsInString(text, character) {}
+function checkIfCharacterIsInString(text, character) {
+  return text.includes(character);
+}
 
 /**
  * isEven()
@@ -95,9 +107,11 @@ function checkIfCharacterIsInString(text, character) {}
  *  isEven(11);
  *  //> false
  */
-function isEven(num) {}
+function isEven(num) {
+  return (num % 2) ? false : true;
+}
 
-/**
+/** 
  * isOdd()
  * ---------------------
  * Returns `true` if the number given is odd. Otherwise, returns `false`.
@@ -111,7 +125,9 @@ function isEven(num) {}
  *  isOdd(11);
  *  //> true
  */
-function isOdd(num) {}
+function isOdd(num) {
+  return (num % 2) ? true : false;
+}
 
 /**
  * isTruthy()
@@ -127,7 +143,10 @@ function isOdd(num) {}
  *  isTruthy(null);
  *  //> false
  */
-function isTruthy(val) {}
+function isTruthy(val) {
+  //return (val) ? true : false;
+  return !(!val); //LogicalNot converts to Boolean, and switches value
+}
 
 /**
  * isFalsy()
@@ -143,7 +162,10 @@ function isTruthy(val) {}
  *  isFalsy(" ");
  *  //> false
  */
-function isFalsy(val) {}
+function isFalsy(val) {
+  //return (val) ? false : true; 
+  return !(val); //LogicalNot converts to Boolean, and switches value
+}
 
 // Do not change the code below.
 module.exports = {
