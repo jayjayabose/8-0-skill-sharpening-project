d3d07ea862cf230e2841f809837140f6d342ec3d
Jay Jayabose <jayabose@gmail.com>
2022-02-18 06:27:50 -0500

    loops tests passed

--- src/06-loops.js
+++ src/06-loops.js
@@ -11,7 +11,13 @@
  *  //> 25
  *  (3 + 4 + 5 + 6 + 7 = 25)
  */
-function rangeSum(min, max) {}
+function rangeSum(min, max) {
+  let sum =0;
+  for (let i=min; i <= max; i++) {
+    sum += i;
+  }
+  return sum;
+}
 
 /**
  * rangeOdd()
@@ -25,7 +31,25 @@ function rangeSum(min, max) {}
  *  rangeOdd(10, 15);
  *  //> [ 15, 13, 11 ]
  */
-function rangeOdd(min, max) {}
+function rangeOdd(min, max) {
+  //create output array
+  let output = [];
+  
+  //add range to array
+  for (i = min; i <= max; i++){
+    output.push(i);
+  }
+  
+  //sort descending
+  output.sort((a, b) => {
+    return b-a;
+  });
+  
+  //filter for odd and return
+  return output.filter(element => {
+    return element % 2;
+  });
+}
 
 /**
  * rangeEveryOther()
@@ -39,7 +63,20 @@ function rangeOdd(min, max) {}
  *  rangeEveryOther(11, 18);
  *  //> [ 11, 13, 15, 17 ]
  */
-function rangeEveryOther(min, max) {}
+function rangeEveryOther(min, max) {
+  //create array to hold range
+  let rangeArray = []
+
+  //add range to array
+  for (let i=min; i<= max; i++){
+    rangeArray.push(i);
+  }
+
+  //filter for every other and return
+  return rangeArray.filter((element, index) => {
+    return !(index % 2);
+  });
+}
 
 /**
  * containsWhileLoop()
@@ -61,7 +98,20 @@ function rangeEveryOther(min, max) {}
  *  containsWhileLoop([ "left", "up", "right" ], "down");
  *  //> false
  */
-function containsWhileLoop(array, target) {}
+function containsWhileLoop(array, target) {
+  //set return value false
+  let foundTarget = false;
+  
+  //set pointer to track array index
+  let i = 0;
+
+  //loop until target is found, or array ends
+  while (foundTarget === false && i < array.length){
+    if (array[i] === target) foundTarget = true;
+    i++
+  }
+  return foundTarget;
+}
 
 /**
  * containsForLoop()
@@ -83,7 +133,12 @@ function containsWhileLoop(array, target) {}
  *  containsForLoop([ "left", "up", "right" ], "down");
  *  //> false
  */
-function containsForLoop(array, target) {}
+function containsForLoop(array, target) {
+  for (let i=0; i < array.length; i++){
+    if (array[i] === target) return true;
+  }
+  return false;
+}
 
 /**
  * targetCount()
@@ -97,7 +152,15 @@ function containsForLoop(array, target) {}
  *  targetCount([ 10, 20, 10, 20, 30 ], 10);
  *  //> 2
  */
-function targetCount(array, target) {}
+function targetCount(array, target) {
+  //set initial count
+  let count = 0;
+
+  for(let i=0; i<array.length; i++){
+    if (array[i] === target) count +=1;
+  }
+  return count;
+}
 
 /**
  * firstIndexFound()
@@ -117,7 +180,12 @@ function targetCount(array, target) {}
  *  firstIndexFound([ "left", "right", "left" ], "up");
  *  //> -1
  */
-function firstIndexFound(array, target) {}
+function firstIndexFound(array, target) {
+  for(let i=0; i<array.length; i++){
+    if (array[i] === target) return i;
+  }
+  return -1;
+}
 
 /**
  * lastIndexFound()
@@ -137,7 +205,16 @@ function firstIndexFound(array, target) {}
  *  lastIndexFound([ "left", "right", "left" ], "up");
  *  //> -1
  */
-function lastIndexFound(array, target) {}
+function lastIndexFound(array, target) {
+  //set initial result
+  let result = -1;
+
+  //loop through all elements
+  for (let i=0; i<array.length; i++){
+    if(array[i] === target) result = i;
+  }
+  return result;
+}
 
 /**
  * timesIndex()
@@ -151,7 +228,17 @@ function lastIndexFound(array, target) {}
  *  //> [ 7 * 0, 10 * 1, 11 * 2 ]
  *  //> [ 0, 10, 22 ]
  */
-function timesIndex(array) {}
+function timesIndex(array) {
+  //create ouput array
+  let output = [];
+
+  //loop through all elements
+  for (let i=0; i<array.length; i++){
+    //add (element * index) to output array
+    output.push(array[i]*i);
+  }
+    return output;
+}
 
 /**
  * cumulativeSum()
@@ -165,7 +252,21 @@ function timesIndex(array) {}
  *  //> [ 5, 5 + 2, 5 + 2 + 9 ]
  *  //> [ 5, 7, 16 ]
  */
-function cumulativeSum(array) {}
+function cumulativeSum(array) {
+  //create output array
+  let output = [];
+
+  //create cumulative sum
+  let cumSum = 0;
+
+  //loop through all elements
+  for (let i=0; i<array.length; i++){
+    //add element to cumultiave sum, then push cumSum to output
+    cumSum += array[i];
+    output.push(cumSum);
+  }    
+  return output;
+}
 
 // Do not change anything below this line.
 module.exports = {
